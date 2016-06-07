af3647c9ed55757e4126c3f9c82751793b0829f2
m99900 <mattsteger@icloud.com>
2016-06-07 09:51:06 -0500

    Update Examples.js

--- Examples.js
+++ Examples.js
@@ -1,1 +1,18 @@
-var data = ""
+var server = {/*our server object form server handler*/}
+server.getroom(/*this will get the room number for your user and all the data in the room*/);
+
+
+server.getline(/*this can once you start the thing can get an empty line for your user*/);
+
+
+
+server.serverupdate(/*this is an array of five objects what ever you need at the time it must be used once you have a line for the user*/);
+
+
+
+server.servermakeroom(/*this is usefull for the main server thing meaning your servers moniter should only be given accses to this function */);
+
+
+
+
+server.roomclear(/* clears the room that you are in*/);
