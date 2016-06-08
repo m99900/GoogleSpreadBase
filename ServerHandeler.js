8a6d8803bb10e5c2ee25b3951ad3b0fd54667008
m99900 <mattsteger@icloud.com>
2016-06-08 09:11:22 -0500

    Update ServerHandeler.js

--- ServerHandeler.js
+++ ServerHandeler.js
@@ -1,9 +1,9 @@
-function server(wich, update, roomtype) { 
+function server(wich, update, roomtype, spreadsheetID) { 
   //start copy
 var server = { // just copy this varible for the ligit function for minipulation 
-getroom:function(Z) {
+getroom:function(Z, id) {
 //this is one of the most vauble of the classfunctions the sheet is for a google sheet then it will be able to minipulate the sheet with server capibility
-var sheet = SpreadsheetApp.openById("1Z4gmv82SSCv760CVF2RDGiykz23gnvEmzmrKE4b7mVY").getSheets()[0];
+var sheet = SpreadsheetApp.openById(id).getSheets()[0];
 var data = sheet.getDataRange().getValues();
 var z = Z;
 var x = [];
@@ -40,9 +40,9 @@ x.push(data[z]);
 //Z is for what line you are in
 return x
 },
-getline:function() {
+getline:function(id) {
 
-var sheet = SpreadsheetApp.openById("1Z4gmv82SSCv760CVF2RDGiykz23gnvEmzmrKE4b7mVY").getSheets()[0];
+var sheet = SpreadsheetApp.openById(id).getSheets()[0];
   var data = sheet.getDataRange().getValues();
 var x = 0;
 while (x !== sheet.getLastRow()) {
@@ -57,7 +57,7 @@ x = null;
 }
 return x;
 },
-serverupdate:function(et) {
+serverupdate:function(et, id) {
 /*
 et = array
 REminder 
@@ -68,7 +68,7 @@ C = (mabye) team or turn
 D = name
 E = ability
 */
-var sheet = SpreadsheetApp.openById("1Z4gmv82SSCv760CVF2RDGiykz23gnvEmzmrKE4b7mVY").getSheets()[0];
+var sheet = SpreadsheetApp.openById(id).getSheets()[0];
 var t666 = ["A", Math.floor(et[0] + 1), ":", "E", Math.floor(et[0] + 1)].join("");
 Logger.log(t666);
 
@@ -80,8 +80,8 @@ var test = [[junck, junck2, "", junck3, ""]];
 range.setValues(test);
  SpreadsheetApp.flush();
 },
-servermakeroom:function(type) {
-var sheet = SpreadsheetApp.openById("1Z4gmv82SSCv760CVF2RDGiykz23gnvEmzmrKE4b7mVY").getSheets()[0];
+servermakeroom:function(type, id) {
+var sheet = SpreadsheetApp.openById(id).getSheets()[0];
 if (type == 1) {
 sheet.appendRow(["roomt1"]);
 sheet.appendRow(["0"]);
@@ -106,8 +106,8 @@ sheet.appendRow(["end"]);
 }
  SpreadsheetApp.flush();
 },
-roomclear:function(roomtype) {
-var sheet = SpreadsheetApp.openById("1Z4gmv82SSCv760CVF2RDGiykz23gnvEmzmrKE4b7mVY").getSheets()[0];
+roomclear:function(roomtype, id) {
+var sheet = SpreadsheetApp.openById(id).getSheets()[0];
 var t666 = ["A", Math.floor(roomtype[0] + 1), ":", "E", Math.floor(roomtype[0] + 1)].join("");
 Logger.log(t666);
 var range = sheet.getRange(t666);
@@ -145,14 +145,14 @@ range.setValues([['0', '', '', '', '']]);
 //
 //
 if (wich == 1) {
-return server.getroom(update);
+return server.getroom(update, spreadsheetID);
 } else if (wich == 2) {
-return server.getline();
+return server.getline(spreadsheetID);
 } else if (wich == 3) {
- server.serverupdate(update);
+ server.serverupdate(update,spreadsheetID);
 } else if (wich == 4) {
-server.servermakeroom(roomtype);
+server.servermakeroom(roomtype, spreadsheetID);
 } else {
-server.roomclear(roomtype);
+server.roomclear(roomtype, spreadsheetID);
 }
 }
